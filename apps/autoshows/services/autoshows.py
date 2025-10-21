d13e5c9c445f36731eea180561873e844b3dbd3a
Artem <artyomkaro4e@gmail.com>
2025-10-21 17:09:47 +0300

    add update and soft delete

--- apps/autoshows/services/autoshows.py
+++ apps/autoshows/services/autoshows.py
@@ -1,28 +1,33 @@
 from typing import Any
 from uuid import UUID
 
-from apps.autoshows.repositories.autoshows import AutoShowsNotFoundException, AutoShowsRepository
+from django.db.models import QuerySet
+
+from apps.autoshows.models import AutoShows
+from apps.autoshows.repositories.autoshows import AutoShowsRepository
 
 
 class AutoShowsService:
     def __init__(self):
         self.repository = AutoShowsRepository()
 
-    def get_all_autoshows(self):
-        """Получить все автосалоны"""
+    def get_all_autoshows(self) -> QuerySet[AutoShows]:
         return self.repository.get_all()
 
-    def get_autoshow_by_id(self, auto_show_id: UUID):
-        """Получить автосалон по ID"""
+    def get_autoshow_by_id(self, auto_show_id: UUID) -> AutoShows | None:
         autoshow = self.repository.get_by_id(auto_show_id)
         if autoshow is None:
-            raise AutoShowsNotFoundException(f"Auto show with id {auto_show_id} not found")
+            return None
         return autoshow
 
-    def get_autoshow_by_id_or_none(self, auto_show_id: UUID):
-        """Получить автосалон по ID или None"""
-        return self.repository.get_by_id(auto_show_id)
+    def get_autoshow_by_id_or_404(self, auto_show_id: UUID) -> AutoShows:
+        return self.repository.get_by_id_or_404(auto_show_id)
 
     def create_autoshow(self, auto_show_data: dict[str, Any]):
-        """Создать новый автосалон"""
         return self.repository.create(auto_show_data)
+
+    def update_autoshow(self, auto_show_id: UUID, auto_show_data: dict[str, Any]):
+        return self.repository.update(auto_show_id, auto_show_data)
+
+    def soft_delete_autoshow(self, auto_show_id: UUID):
+        return self.repository.soft_delete(auto_show_id)
