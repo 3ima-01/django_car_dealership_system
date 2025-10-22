eca8a462628b252389daf4cda9db3d200b73e9f1
Artem <artyomkaro4e@gmail.com>
2025-10-22 09:27:57 +0300

    move api versioning on one level higher

--- apps/autoshows/api/urls.py
+++ apps/autoshows/api/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from apps.autoshows.api.views.autoshows import AutoShowsAPIView
 
+app_name = "autoshows"
+
 urlpatterns = [
-    path("api/v1/auto-shows/", AutoShowsAPIView.as_view(), name="auto-shows-list"),
-    path("api/v1/auto-shows/<uuid:id>/", AutoShowsAPIView.as_view(), name="auto-shows-detail"),
+    path("auto-shows/", AutoShowsAPIView.as_view(), name="auto-shows-list"),
+    path("auto-shows/<uuid:id>/", AutoShowsAPIView.as_view(), name="auto-shows-detail"),
 ]
--- car_dealership_system/urls.py
+++ car_dealership_system/urls.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from apps.autoshows.api.urls import urlpatterns as autoshows_urlpatterns
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include("rest_framework.urls", namespace="rest_framework")),
+    path("api/v1/", include("apps.autoshows.api.urls")),
 ]
-
-urlpatterns += autoshows_urlpatterns
