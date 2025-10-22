d97ccf25f49fdd66bf75612ba53d08949e1fbba8
Artem <artyomkaro4e@gmail.com>
2025-10-22 09:29:08 +0300

    fix id. add max_length

--- apps/common/models.py
+++ apps/common/models.py
@@ -1,13 +1,15 @@
+from uuid import uuid4
+
 from django.db import models
 
 
 class Cars(models.Model):
-    id = models.UUIDField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     year = models.IntegerField()
     brand = models.CharField(max_length=64)
     model = models.CharField(max_length=64)
     color = models.CharField(max_length=64)
-    body_type = models.CharField()
+    body_type = models.CharField(max_length=64)
     engine_type = models.CharField(max_length=64)
     horse_power = models.IntegerField()
     properties = models.JSONField(null=True, default=dict)
