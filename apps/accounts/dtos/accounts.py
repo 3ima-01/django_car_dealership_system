3533f493570fbf4e5f17711bf3ead97681c3199c
Artem <artyomkaro4e@gmail.com>
2025-11-06 10:52:30 +0300

    delete dtos

--- apps/accounts/dtos/accounts.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-class CustomerRegisterDTO(BaseModel):
-    email: str
-    password: str
--- apps/accounts/dtos/email.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-class EmailVerifyDTO(BaseModel):
-    email: str
-    token: str
