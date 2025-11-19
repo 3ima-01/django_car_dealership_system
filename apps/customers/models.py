201764810f8a1860994d9109a521e944e4eee135
Artem <artyomkaro4e@gmail.com>
2025-11-19 13:55:07 +0300

    add offer status choises

--- apps/customers/models.py
+++ apps/customers/models.py
@@ -3,11 +3,9 @@ from uuid import uuid4
 from django.core.validators import MinValueValidator
 from django.db import models
 
-from apps.accounts.models import Customers
-
 
 class Profiles(models.Model):
-    customer_id = models.OneToOneField(Customers, on_delete=models.CASCADE)
+    customer_id = models.OneToOneField("accounts.Customers", on_delete=models.CASCADE, related_name="profile")
     first_name = models.CharField(max_length=128)
     last_name = models.CharField(max_length=128)
     phone = models.CharField(max_length=17)
@@ -15,7 +13,14 @@ class Profiles(models.Model):
 
 
 class Offers(models.Model):
+    STATUSES = (
+        ("ACTIVE", "ACTIVE"),
+        ("CANCELED", "CANCELED"),
+        ("COMPLETED", "COMPLETED"),
+    )
+
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    customer_id = models.ForeignKey(Customers, on_delete=models.CASCADE)
+    customer_id = models.ForeignKey("accounts.Customers", on_delete=models.CASCADE)
     model = models.CharField(max_length=64)
     max_price = models.DecimalField(max_digits=12, decimal_places=2, validators=[MinValueValidator(0)])
+    status = models.CharField(max_length=32, choices=STATUSES)
